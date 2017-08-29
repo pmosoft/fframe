586e19262ec6da7a5107caa65d3ae8d97b372016
Joseph Pi <pmosoft@gmail.com>
2017-08-29 22:44:54 +0900

    ..

--- src/main/webapp/app/dams/term/TermListModel.js
+++ src/main/webapp/app/dams/term/TermListModel.js
@@ -8,7 +8,27 @@ Ext.define('fframe.dams.term.TermListModel', {
      }
     ,stores : {
         TermList : {
-    	    type : 'TermList'
+             fields : ['COL_NM'
+                       ,'COL_HNM'
+                       ,'COL_DESC'
+                       ,'COL_ABBR_HNM'
+                       ,'INFO_TYPE_NM'
+                       ,'COL_STS_CD'
+                       ,'COL_STS_NM'
+                       ,'REG_DTM'
+                       ,'REG_USR_ID'
+                       ,'UPD_DTM'
+                       ,'UPD_USR_ID'
+                       ]
+             ,proxy : {
+                  type : 'ajax'              
+                 ,url : '/dams/term/selectExtTermList'
+                 ,reader : {
+                      type : 'json'
+                     ,rootProperty : 'data'  
+                 }
+             } 
         }
      } 
 });
+
