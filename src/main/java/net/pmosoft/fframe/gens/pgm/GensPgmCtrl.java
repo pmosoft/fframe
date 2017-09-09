1b598fd2df2ca8512522e5e90d3321a7ef3e8091
Joseph Pi <pmosoft@gmail.com>
2017-09-10 01:18:04 +0900

    ..

---
+++ src/main/java/net/pmosoft/fframe/gens/pgm/GensPgmCtrl.java
@@ -0,0 +1,28 @@
+package net.pmosoft.fframe.gens.pgm;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+ 
+/**
+ * @author Administrator
+ *
+ */
+@RestController
+public class GensPgmCtrl {
+
+//    @Autowired
+//    private GensPgmSrv GensPgmSrv;
+ 
+
+    
+}
