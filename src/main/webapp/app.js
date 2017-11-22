87d0b49f76031745f30dc214e66867ee3e3d76ce
lifedomy <lifedomy@gmail.com>
2017-11-23 03:00:04 +0900

    ..

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -8,8 +8,9 @@ Ext.application({
     ,extend: 'fframe.Application'
     ,requires: [
          'fframe.main.MainFrameView'
-        ,'fframe.comm.CommCombo' 
-     ]
+        ,'Ext.form.field.ComboBox'
+        ,'fframe.comm.CommCombo'
+    ]
     // The name of the initial view to create. With the classic toolkit this class
     // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
     // modern toolkit, the main view will be added to the Viewport.
