b8f86166c99bff5e6fb0f74ac96814e2dff792e9
lifedomy <lifedomy@gmail.com>
2017-12-27 00:42:38 +0900

    ..

--- src/main/java/net/pmosoft/fframe/comm/App.java
+++ src/main/java/net/pmosoft/fframe/comm/App.java
@@ -5,6 +5,6 @@ public class App {
 	public static final String webPath = "C:/A-SAFE/workspace/a-safe/src/main/webapp/fframe/WEB-INF/jsp/";
 	public static final String javaPath = "C:/A-SAFE/workspace/a-safe/src/main/java/amls/";
     public static final String excelPath = "C:/fframe/workspace/fframe/src/main/webapp/fframe/files/excel/";
-
+ 
 }
 
